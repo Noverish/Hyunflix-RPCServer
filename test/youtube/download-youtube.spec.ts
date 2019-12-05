bf2835d6a839b487103f5711d8f41cb93b6a518a
Hyunsub Kim <embrapers263@gmail.com>
2019-12-05 19:45:36 +0900

    Added tests

---
+++ test/youtube/download-youtube.spec.ts
@@ -0,0 +1,27 @@
+import { assert } from 'chai';
+import 'mocha';
+
+import { downloadYoutube } from '@src/functions/youtube';
+import { YoutubeStatus } from '@src/models';
+
+const STATUS_MOCK: YoutubeStatus = { progress: 0, total: 0, speed: 0, eta: 0 };
+
+const tests = [
+  {
+    title: '[MV] IU(아이유) _ Blueming(블루밍)',
+    url: 'https://www.youtube.com/watch?v=D1PvIWdJ8xo',
+    expected: '/Musics/download/[MV] IU(아이유) _ Blueming(블루밍).webm'
+  },
+]
+
+describe('downloadYoutube', () => {
+  for (const test of tests) {
+    it(test.title, async function () {
+      this.timeout(0);
+      const path = await downloadYoutube(test.url, (status: YoutubeStatus) => {
+        assert.hasAllKeys(status, STATUS_MOCK);
+      });
+      assert.deepEqual(path, test.expected);
+    })
+  }
+})
---
+++ test/youtube/list-youtube-playlist.spec.ts
@@ -0,0 +1,27 @@
+import { assert } from 'chai';
+import 'mocha';
+
+import { listYoutubePlaylist } from '@src/functions/youtube';
+
+const tests = [
+  {
+    title: 'playlist url',
+    url: 'https://www.youtube.com/playlist?list=PLHFlHpPjgk704bliuD3qP37EzVNNypOJi',
+    expected: [{ id: '7Glc2mHLPRM', title: 'Look what you can do with iPadOS \u2014 Apple' }]
+  },
+  {
+    title: 'video url with playlist id',
+    url: 'https://www.youtube.com/watch?v=7Glc2mHLPRM&list=PLHFlHpPjgk704bliuD3qP37EzVNNypOJi&index=2&t=0s',
+    expected: [{ id: '7Glc2mHLPRM', title: 'Look what you can do with iPadOS \u2014 Apple' }]
+  },
+]
+
+describe('listYoutubePlaylist', () => {
+  for (const test of tests) {
+    it(test.title, async function () {
+      this.timeout(0);
+      const items = await listYoutubePlaylist(test.url);
+      assert.deepEqual(items, test.expected);
+    })
+  }
+})
